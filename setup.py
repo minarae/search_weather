e1cb92a26db483f1313a4bb765dce92cf03f290a
minarae <minarae@gmail.com>
2024-10-23 07:17:04 +0900

    version 변경

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ class PostInstallCommand(install):
 
 setup(
     name="search_weather",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     install_requires=[
